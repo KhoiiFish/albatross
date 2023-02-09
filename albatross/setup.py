da6ecd4395cdfaf5679d24bacb6fa7a079dc289d
Kevin Nguyen <kevinnguyen.sw@gmail.com>
2023-02-09 14:25:08 +1000

    start setup.py

---
+++ albatross/setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+import warnings
+
+
+dir_here = os.path.abspath(os.path.dirname(__file__))
+
+# We strive to keep dependencies as flexible as possible, whilst avoiding depdency conflicts in breaking versions.
+install_requires = [
+    # We pin to exact versions here so we don't pickup breaking changes accidentally
+    "Django==4.1.3",
+    "black==22.3.0",
+    "dataclasses",
+]
+
+
+setuptools.setup(
+    name="albatross",
+    description="Albatross Planner",
+    long_description="Plan your itinerary with Albatross",
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author="Kevin Nguyen",
+    author_email="kevinnguyen.sw@gmail.com",
+    packages=setuptools.find_packages(exclude="tests",),
+    include_package_data=True,
+    install_requires=install_requires,
+)
